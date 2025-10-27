d684c718c3c9b41d8507ec41079990b067f3e0e0
Meddy204 <Meddyally121@gmail.com>
2025-10-27 22:08:00 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~2lgj2IbD#UEe5xEhhJS7C7NKbO4T20uDlznT5A06j_KRzTaUTSTcf51a7b3c1502ee99",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "255622731311", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "Africa/Daresalaam" //put your country timeZone....leave blank if u don't know.
 };
